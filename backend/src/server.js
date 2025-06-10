f313b6ec240dde3dc482be0e2a197dd25a4eee63
Vishal Rajput <vishalrajput.7vr@gmail.com>
2025-06-10 21:05:01 +0530

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,28 +5,40 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, // If you're using cookies or sessions
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true, // If you're using cookies or sessions
+    })
+  );
+}
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.get("/start", (req, res) => {
   res.status(200).json({ message: "welcome to scribble" });
 });
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+// in production, there's no localhost so we have to make this dynamic
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api/v1"
+      : "/api/v1",
   withCredentials: true,
 });
